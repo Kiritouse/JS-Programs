affe9fa08c81a2d6484e265abfa44f0cccd8ff56
Kiritouse <2496305505@qq.com>
2023-12-03 18:17:04 +0800

    第一次尝试油猴

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("firstVersion");
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+})();
